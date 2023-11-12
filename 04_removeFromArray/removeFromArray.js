e07a8e492cd3353a336e0bf8d849df83ffb76c63
Michał Pawlak <michal.pawlak001@gmail.com>
2023-11-12 09:37:21 +0100

    do exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,...nums) {
+        for (let i = 0; i < nums.length; i++) {
+            if(arr.indexOf(nums[i])>=0){
+            arr.splice(arr.indexOf(nums[i]),1);
+            }
+        }
+        return arr;
 };
 
 // Do not edit below this line
