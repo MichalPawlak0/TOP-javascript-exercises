cb7b45a3d87dc9b88b5766cfbcb7ebf0bf07ae2e
Michał Pawlak <michal.pawlak001@gmail.com>
2023-11-12 09:45:52 +0100

    do exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if((num1<0||num2<0)||(typeof num1!=="number"||typeof num2!=="number")){
+        return "ERROR";
+    }
+    
+    let start,finish;
+    if(num1>num2){
+        start=num2;
+        finish=num1;
+    }else{
+        start=num1;
+        finish=num2;
+    }
+    let sum=0;
+    for (let i = start; i <= finish; i++) {
+        sum +=i;        
+    }
+    return sum;
 };
 
 // Do not edit below this line
