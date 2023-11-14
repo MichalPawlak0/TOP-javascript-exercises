87f0f818672cb2075073da9f5cddeab3b0f6cdd9
Michał Pawlak <michal.pawlak001@gmail.com>
2023-11-14 22:08:11 +0100

    do exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1+num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1-num2
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+	 return args.reduce((sum,arg)=>(sum+arg),0);
+}
+
+const multiply = function(args) {
+  return args.reduce((total,arg)=>{return total*arg},1)
 };
 
-const multiply = function() {
-
+const power = function(num1,num2) {
+  if(num2===0) return 1
+	return num1**num2
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+  if(n===0) return 1;
+	return n * factorial(n-1)
 };
 
 // Do not edit below this line
