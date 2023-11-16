b9afa6fb07876e9bea56d96253be64a6cbe87c84
Michał Pawlak <michal.pawlak001@gmail.com>
2023-11-16 15:12:16 +0100

    do exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+let youngToOld = arr.sort((a,b)=>{
+    if(!a.yearOfDeath){
+        const date = new Date()
+        a.yearOfDeath = date.getFullYear()
+    }else if (!b.yearOfDeath){
+        const date = new Date()
+        b.yearOfDeath = date.getFullYear()
+    }
 
+    return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth)
+})
+console.log(youngToOld)
+return youngToOld[0]
 };
 
 // Do not edit below this line
